11f2cffd6610c4240eb290cab6c2f85281cb198c
lbqds <lbqds@outlook.com>
2024-06-04 11:35:48 +0800

    Custom provider

---
+++ src/components/ConnectModal.tsx
@@ -0,0 +1,57 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import { useConnect, useConnectSettingContext } from '@alephium/web3-react';
+import { Account } from '@alephium/web3';
+import { Button, Modal, Box } from '@mui/material';
+
+export function ConnectModal() {
+  const [opened, setOpened] = useState(false)
+  const [connectClicked, setConnectClicked] = useState(false)
+  const context = useConnectSettingContext()
+
+  const [account, setAccount] = useState<Account | undefined>(undefined)
+
+  const { connect, disconnect } = useConnect()
+
+  useEffect(() => {
+    if (connectClicked && opened) {
+      setConnectClicked(false)
+      connect().then((account) => {
+        setAccount(account)
+        setOpened(false)
+      })
+    }
+  }, [connectClicked, opened])
+
+  const onConnect = useCallback((id: 'injected' | 'walletConnect') => {
+    context.setConnectorId(id)
+    setConnectClicked(true)
+  }, [])
+
+  const onDisconnect = useCallback(async () => {
+    await disconnect()
+    setAccount(undefined)
+  }, [disconnect])
+
+  return (
+    <div>
+      {account !== undefined ? (
+        <div>
+          <Button variant="contained" onClick={onDisconnect}>Disconnect</Button>
+        </div>
+      ) : (
+        <div>
+          <Button variant="contained" onClick={() => setOpened(true)}>Connect</Button>
+          <Modal
+            open={opened}
+            onClose={() => setOpened(false)}
+          >
+            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', padding: 20 }}>
+              <Button variant="contained" onClick={() => onConnect('injected')}>Extension Wallet</Button>
+              <Button variant="contained" sx={{ mt: 2 }} onClick={() => onConnect('walletConnect')}>Wallet Connect</Button>
+            </Box>
+          </Modal>
+        </div>
+      )}
+    </div>
+  );
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { AlephiumWalletProvider } from '@alephium/web3-react'
-import { tokenFaucetConfig } from '@/services/utils'
+import { CustomProvider } from '@/components/CustomProvider'
 
 export default function App({ Component, pageProps }: AppProps) {
 
   return (
-    <AlephiumWalletProvider theme="web95" network={tokenFaucetConfig.network} addressGroup={tokenFaucetConfig.groupIndex}>
+    <CustomProvider>
       <Component {...pageProps} />
-    </AlephiumWalletProvider>
+    </CustomProvider>
   )
 }
